97373a519fe176c4f5b9cca9c9ffd85550a3f252
CTrando <cameron.trando@yahoo.com>
2017-04-03 19:29:43 -0700

    Fixing div not being included in parent

--- js/bubble.js
+++ js/bubble.js
@@ -1,35 +1,42 @@
-
 /**
  * Created by Cameron on 4/3/2017.
  */
 
 function Bubble() {
-    this.pos = createVector(random(0,windowWidth), random(0, windowHeight));
-    this.vel = createVector(random(0,10), random(0,10));
+    this.pos = createVector(random(0, windowWidth), random(0, windowHeight));
+    this.vel = createVector(random(0, 10), random(0, 10));
 
-    this.radius = random(50,100);
+    this.radius = random(50, 100);
 
-    this.draw = function(){
+    this.reverseX = function (vec) {
+        vec.set(vec.x * -1, vec.y);
+    };
+
+    this.reverseY = function (vec) {
+        vec.set(vec.x, vec.y * -1);
+    };
+
+    this.draw = function () {
         ellipse(this.pos.x, this.pos.y, this.radius, this.radius);
     };
 
-    this.update = function() {
+    this.update = function () {
         this.pos.x += this.vel.x;
         this.pos.y += this.vel.y;
 
-        if(this.pos.x > windowWidth || this.pos.x < 0) {
-            this.vel.set(this.vel.x*-1, this.vel.y);
+        if (this.pos.x > windowWidth || this.pos.x < 0) {
+            this.reverseX(this.vel);
         }
 
-        if(this.pos.y > windowHeight || this.pos.y < 0) {
-            this.vel.set(this.vel.x, this.vel.y*-1);
+        if (this.pos.y > windowHeight || this.pos.y < 0) {
+            this.reverseY(this.vel);
         }
     }
 }
 
 function initBubbles() {
     var numBubbles = random(minBubbles, maxBubbles);
-    for(var i = 0; i< numBubbles; i++){
+    for (var i = 0; i < numBubbles; i++) {
         bubbles.push(new Bubble());
     }
 }
@@ -40,20 +47,20 @@ var canvas;
 var bubbles = [];
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
-    canvas.background(255,0,255);
-    canvas.position(0,0);
+    canvas.background(255, 0, 255);
+    canvas.position(0, 0);
     canvas.style("z-index", "-1");
-    canvas.background(255,255,255);
+    canvas.background(255, 255, 255);
     frameRate(30);
 
-    stroke(color(0,0,255));
+    stroke(color(0, 0, 255));
 
     initBubbles();
 }
 
 function draw() {
-    canvas.background(255,255,255);
-    for(var i = 0; i< bubbles.length; i++){
+    canvas.background(255, 255, 255);
+    for (var i = 0; i < bubbles.length; i++) {
         bubbles[i].update();
         bubbles[i].draw();
     }
